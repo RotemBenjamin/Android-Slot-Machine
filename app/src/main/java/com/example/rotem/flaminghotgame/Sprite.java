0462e66a67b43eabf700b3d8e2a06da18c3bce6f
rotem benjamin <rotemb1985@gmail.com>
2016-03-23 03:48:40 +0200

    add getter and setter to Sprite

--- app/src/main/java/com/example/rotem/flaminghotgame/Sprite.java
+++ app/src/main/java/com/example/rotem/flaminghotgame/Sprite.java
@@ -23,4 +23,60 @@ public class Sprite
         this.height = height;
         this.bitmap = bitmap;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
 }
