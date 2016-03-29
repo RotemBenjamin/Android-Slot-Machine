b4c8b5db0e860bf857b2a73ae85421f7f71e5a8f
rotem benjamin <rotemb1985@gmail.com>
2016-03-29 14:03:54 +0300

    add basic values to MyStatic

--- app/src/main/java/com/example/rotem/flaminghotgame/MyStatic.java
+++ app/src/main/java/com/example/rotem/flaminghotgame/MyStatic.java
@@ -5,5 +5,9 @@ package com.example.rotem.flaminghotgame;
  */
 public class MyStatic
 {
-    
+    public static final int NUMBER_OF_FRUITS_IN_ONE_WHEEL = 20;
+    public static final int[] WHEEL_VALUES_ARRAY = {-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public static final int BOARD_ROWS = 4, BOARD_COLS = 5;
+    public static int[][] THE_MAIN_BOARD = { {0,0,0,0,0} , {0,0,0,0,0} , {0,0,0,0,0} , {0,0,0,0,0} };
 }
