5678287a982693c53475b75e414de03f7733aa18
rotem benjamin <rotemb1985@gmail.com>
2016-04-04 12:40:47 +0300

    add basic Override methods to WheelView

--- app/src/main/java/com/example/rotem/flaminghotgame/WheelView.java
+++ app/src/main/java/com/example/rotem/flaminghotgame/WheelView.java
@@ -1,6 +1,7 @@
 package com.example.rotem.flaminghotgame;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +10,8 @@ import android.view.View;
  */
 public class WheelView extends View
 {
+    private int right = 0, top = 0, width = 0, height = 0;
+
     public WheelView(Context context) {
         super(context);
         init(context, null, 0);
@@ -27,7 +30,24 @@ public class WheelView extends View
     /**
      * Initialize all the variables
      */
-    private void init(Context context, AttributeSet attrs, int defStyle) {
+    private void init(Context context, AttributeSet attrs, int defStyle)
+    {
 
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        right = getPaddingRight();
+        top = getPaddingTop();
+        width = w - (getPaddingLeft() + getPaddingRight());
+        height = h - (getPaddingTop() + getPaddingBottom());
     }
 }
