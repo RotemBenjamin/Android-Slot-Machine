3c44313b7f4b30348c54736aa713ef4382d6b9db
rotem benjamin <rotemb1985@gmail.com>
2016-04-08 13:08:39 +0300

    override all the activity life cycle

--- app/src/main/java/com/example/rotem/flaminghotgame/MainActivity.java
+++ app/src/main/java/com/example/rotem/flaminghotgame/MainActivity.java
@@ -11,4 +11,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * private method that reset all the components
+     */
+    private void resetThings()
+    {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        resetThings();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resetThings();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        resetThings();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        resetThings();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        resetThings();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        resetThings();
+    }
 }
